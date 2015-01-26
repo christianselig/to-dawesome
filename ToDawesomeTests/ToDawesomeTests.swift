5418de7993c5dd177d993637b188eb2f52cee3aa
Christian Selig <me@christianselig.com>
2015-01-26 17:22:09 -0400

    Initial Commit

---
+++ ToDawesomeTests/ToDawesomeTests.swift
@@ -0,0 +1,36 @@
+//
+//  ToDawesomeTests.swift
+//  ToDawesomeTests
+//
+//  Created by Christian Selig on 2015-01-26.
+//  Copyright (c) 2015 Christian Selig. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class ToDawesomeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
